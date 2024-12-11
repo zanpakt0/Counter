e23cf01aa1a287722248dceebf5044ace67b9eda
Konstantin Zhukov <k.zhukov@vk.team>
2024-12-11 04:46:49 +0300

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Zhukov Konstantin on 11.12.2024.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
