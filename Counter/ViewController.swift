8ac89e27092a48511c877521f2ecc0ea94e7a528
Konstantin Zhukov <k.zhukov@vk.team>
2024-12-11 05:21:42 +0300

    Add new login at UI with increase counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counter: Int = 0
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var Counter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Counter.text = "Значение счётчика: \(counter)"
     }
 
-
+    @IBAction func upCounter(_ sender: Any) {
+        counter += 1
+        Counter.text = "Значение счётчика: \(counter)"
+    }
+    
 }
 
